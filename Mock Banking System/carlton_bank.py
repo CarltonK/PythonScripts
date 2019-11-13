31bade0b75987f36fe8b9d6fbe8292660bbbaadf
Mark Carlton <mcarlton33@gmail.com>
2019-11-13 17:52:30 +0300

    Banking systems revamped

--- Mock Banking System/carlton_bank.py
+++ Mock Banking System/carlton_bank.py
@@ -2,7 +2,9 @@
 Account class as Base class
 '''
 class Account():
-
+    '''
+    Account class as Base class
+    '''
     def __init__(self, acc_num, balance=0):
         self.acc_num = acc_num
         self.balance = balance
@@ -12,6 +14,7 @@ class Account():
         '''
         Open a bank account to proceed
         '''
+        print()
         print('WELCOME TO CARLTON BANK')
         while True:
             try:
@@ -27,6 +30,7 @@ class Account():
                     print('...ACCOUNT DETAILS...')
                     print('Account Number: {}\nAccount Balance: {}'.format(self.acc_num, self.balance))
                     break
+
 
     def deposit_funds(self):
         while True:
@@ -49,7 +53,7 @@ class Account():
                 print('Invalid entry. Please enter a number.')
             else:
                 if withdraw > self.balance:
-                    print('...ERROR...\nINSUFFICIENT FUNDS\nAvailable balance: {}'.format(self.balance))
+                    print('...ERROR...ERROR...\nINSUFFICIENT FUNDS\nAvailable balance: {}'.format(self.balance))
                 else:
                     self.balance -= withdraw
                     print()
@@ -60,16 +64,6 @@ class Account():
     def __str__(self):
         return 'Account Number: {}\nAccount Balance: {}'.format(self.acc_num, self.balance)
 
-    '''
-    def __del__(self):
-        choice = input('Do you want to close your account? ')
-        if choice[0].lower() == 'y':
-            del(self)
-            print('Account {} has been closed'.format(self.acc_num))
-        else:
-            print('Account not deleted')
-    '''
-
 
 def generate_id():
     '''
@@ -78,13 +72,13 @@ def generate_id():
     # Random selection
     import random
 
-    lower_case = ['a','b','c','d','e','f','g','h','i','j','k','l','m'
-                  'n','o','p','q','r','s','t','u','v','w','x','y','z']
+    lower_case = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
+                  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-    upper_case = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
-                  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    upper_case = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-    number_char = ['0','1','2','3','4','5','6','7','8','9']
+    number_char = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
     vi_length = 10
 
@@ -109,27 +103,12 @@ Savings account
 '''
 class SavingsAccount(Account):
 
-    def __init__(self, acc_num = generate_id(), balance = 0):
+    def __init__(self, acc_num=generate_id(), balance=0):
         super().__init__(acc_num, balance)
 
-    def open_account(self):
-        '''
-        Open a savings account to proceed
-        '''
-        print('WELCOME TO CARLTON BANK')
-        while True:
-            try:
-                request = int(input('Minimum deposit fee is 1000.\nHow much do you want to deposit? '))
-            except ValueError:
-                print('Invalid entry. Please enter a number')
-            else:
-                if request < 1000:
-                    print('Minimum amount is 1000')
-                else:
-                    self.balance += request
-                    break
 
     def __str__(self):
+        print()
         return '...SAVINGS ACCOUNT...\n{}'.format(Account.__str__(self))
 
 
@@ -138,34 +117,73 @@ class SavingsAccount(Account):
 Current Account
 '''
 class CurrentAccount(Account):
-    def __init__(self, acc_num = generate_id(), balance = 0):
+
+    def __init__(self, acc_num=generate_id(), balance=0):
         super().__init__(acc_num, balance)
 
-    def open_account(self):
-        '''
-        Open a current account to proceed
-        '''
-        print('WELCOME TO CARLTON BANK')
-        while True:
-            try:
-                request = int(input('Minimum deposit fee is 1000.\nHow much do you want to deposit? '))
-            except ValueError:
-                print('Invalid entry. Please enter a number')
-            else:
-                if request < 1000:
-                    print('Minimum amount is 1000')
-                else:
-                    self.balance += request
-                    break
 
     def __str__(self):
+        print()
         return '...CURRENT ACCOUNT...\n{}'.format(Account.__str__(self))
 
-save = SavingsAccount()
-print(save)
+'''
+Business Account
+'''
+class BusinessAccount(Account):
+    def __init__(self, acc_num=generate_id(), balance=0):
+        super().__init__(acc_num, balance)
 
-curr = CurrentAccount()
-print(curr)
+    def __str__(self):
+        print()
+        return '...BUSINESS ACCOUNT...\n{}'.format(Account.__str__(self))
 
-save.deposit_funds()
-curr.withdraw_funds()
+
+class BankClient():
+
+    def __init__(self, client_name, client_pin):
+        self.client_name = client_name
+        self.client_pin = client_pin
+        self.accts = {'CURRENT':[], 'SAVINGS':[], 'BUSINESS':[]}
+
+    def __str__(self):
+        return self.client_name
+
+    def open_savings(self):
+        self.accts['SAVINGS'].append(SavingsAccount())
+
+    def open_current(self):
+        self.accts['CURRENT'].append(CurrentAccount())
+
+    def open_business(self):
+        self.accts['BUSINESS'].append(BusinessAccount())
+
+    def total_deposits(self):
+        total = 0
+        for acct in self.accts['CURRENT']:
+            print(acct)
+            total += acct.balance
+        for acct in self.accts['SAVINGS']:
+            print(acct)
+            total += acct.balance
+        for acct in self.accts['BUSINESS']:
+            print(acct)
+            total += acct.balance
+        print()
+        print('Grand Total: {}'.format(total))
+
+'''
+Program flow
+'''
+name = input('Enter your name: ')
+pin = input('Enter your PIN: ')
+
+# Initialize Client
+client = BankClient(name, pin)
+
+# Open Bank Accounts
+client.open_savings()
+client.open_current()
+client.open_business()
+
+# Client Summary
+client.total_deposits()
